f36f78e47401e23a3d8772c34ff811198eb6bb0b
Lukas Rudvalis <lukas.rudvalis@gmail.com>
2023-04-07 11:32:45 +0300

    tags radio

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -3,13 +3,20 @@ import PostsList from '../components/postsComponents/PostsList';
 import useGetData from '../hooks/useGetData';
 import Alert from '../components/ui/Alert';
 import styled from 'styled-components';
+import { useState } from 'react';
 
 const Wrap = styled.div`
   margin-top: 3rem;
   margin-bottom: 3rem;
 `;
+const Flex = styled.div`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+`;
 
 function PostsPage() {
+  const [activeFilterVal, setActiveFilterVal] = useState('all');
   // 2 sukrti state klaidai errorPosts
   const [allPosts, setAllPosts, error, isLoading] = useGetData(
     'http://localhost:5000/posts',
@@ -25,6 +32,13 @@ function PostsPage() {
   // errorText yra lygus tusciai kabutei, bet jei errorPosts yra lygus ERR_NETWORK
   // tada jis lygus "There was a network error, try agail later"
 
+  function getTags() {
+    const allTags2dArr = allPosts.map((obj) => obj.tags);
+    const allTags1dArr = [].concat(...allTags2dArr);
+    const uniqueTags = ['all', ...new Set(allTags1dArr)];
+    return uniqueTags;
+  }
+
   return (
     <Container>
       {isLoading && <Alert>Loading...</Alert>}
@@ -35,6 +49,23 @@ function PostsPage() {
         <h1>PostsPage</h1>
         <p>Welcome to PostsPage</p>
       </Wrap>
+      <fieldset>
+        <legend>Filter by</legend>
+        <Flex>
+          {getTags().map((tag) => (
+            <div key={tag}>
+              <input
+                onChange={() => setActiveFilterVal(tag)}
+                type="radio"
+                name="tags"
+                value={tag}
+                id={tag}
+              />
+              <label htmlFor={tag}>{tag.trim()}</label>
+            </div>
+          ))}
+        </Flex>
+      </fieldset>
       {/* 5 sukrti ir atvaizduoti styled komponenta jei errorText yra ne tuscia kabute */}
       <PostsList posts={allPosts} />
     </Container>
